40f670fd0c320f0ce42a6e4d6c9101b97eaab50d
tun43p <git@tun43p.com>
2022-10-22 16:47:35 +0200

    feat: add some style

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,10 @@ type Person = {
 
 type Team = string;
 
-export type Result = {
+export type Results = {
   a: Person;
   b: Person;
-};
+}[];
 
 // TODO: Create an input on the website to do this
 const statusList = ["DUOA", "Manager", "PM"];
@@ -53,8 +53,8 @@ async function get(file: File): Promise<{ persons: Person[]; teams: Team[] }> {
 }
 
 // TODO: Add try catch
-function parse(data: { persons: Person[]; teams: Team[] }): Result[] {
-  const results: Result[] = [];
+function parse(data: { persons: Person[]; teams: Team[] }): Results {
+  const results: Results = [];
 
   for (let i = 0; i < data.teams.length; i++) {
     const a = data.persons.filter((person) => person.team.index === i);
@@ -74,8 +74,33 @@ function parse(data: { persons: Person[]; teams: Team[] }): Result[] {
   return results;
 }
 
-export async function generate(file: File): Promise<Result[]> {
-  const data = await get(file);
-  const results = parse(data);
-  return results;
+export async function generate(file: File): Promise<Results> {
+  const button = document.getElementById("button");
+  const header = document.getElementById("header");
+  const label = document.getElementById("label");
+  const logo = document.getElementById("logo");
+  const title = document.getElementById("title");
+
+  try {
+    const data = await get(file);
+    const results = parse(data);
+
+    if (header != null) header.style.height = "4rem";
+    if (logo != null) logo.style.fontSize = "2rem";
+
+    if (title != null) {
+      title.style.fontSize = "1.5rem";
+      title.style.marginBottom = "1rem";
+    }
+
+    if (label != null) label.textContent = "Upload an other CSV file";
+    if (button != null) button.style.backgroundColor = "#27ae60";
+
+    return results;
+  } catch (error) {
+    if (label != null) label.textContent = "Upload an other CSV file";
+    if (button != null) button.style.backgroundColor = "#e74c3c";
+
+    throw error;
+  }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,50 @@
 <script lang="ts">
-  import { generate } from "$lib/files";
+  import { generate } from "../app";
+  import "../app.css";
 
   let files: FileList;
 </script>
 
-<input type="file" accept=".csv" title="Upload file" bind:files />
+<header id="header" class="header">
+  <p id="logo" class="logo">Randmeet</p>
+</header>
 
-{#if files && files[0]}
-  {#await generate(files[0])}
-    <p>...loading</p>
-  {:then results}
-    {#if results.length !== 0}
-      {#each results as result}
-        <p>{result.a.name} - {result.b.name}</p>
-      {/each}
+<main class="main">
+  <h1 id="title" class="title">Generate randomly your meetings</h1>
+
+  <div id="button" class="button">
+    <label id="label" for="file">Upload your CSV file</label>
+    <input
+      type="file"
+      accept=".csv"
+      id="file"
+      style="display:none;"
+      bind:files
+    />
+  </div>
+
+  <div class="content">
+    {#if files && files[0]}
+      {#await generate(files[0])}
+        <p class="loader">...loading</p>
+      {:then results}
+        {#if results.length !== 0}
+          <table class="table">
+            <tr>
+              <th>A</th>
+              <th>B</th>
+            </tr>
+            {#each results as result}
+              <tr>
+                <td>{result.a.name}</td>
+                <td> {result.b.name}</td>
+              </tr>
+            {/each}
+          </table>
+        {/if}
+      {:catch error}
+        <p class="error">{error.message}</p>
+      {/await}
     {/if}
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-{/if}
+  </div>
+</main>
